3c55d4e6eb99db3860b6357a9eea01936e7d1313
Omar Monrroy <ogcm.kael@hotmail.com>
2020-01-22 18:56:01 -0600

    Implementando Dotenv

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express = require('express');
+import * as path from 'path';
+import { config } from 'dotenv';
 
 export default class Server {
   public app: express.Application;
 
   constructor(private port: number) {
     this.app = express();
+    config({ path: path.resolve(__dirname, '../.env') });
   }
 
   start(callback: any) {
