942c7b817f56b14c68461771f47521a119564aaf
Omar Monrroy <ogcm.kael@hotmail.com>
2020-01-22 20:36:01 -0600

    Implementando body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Server from './server/server';
+import * as bodyParser from 'body-parser';
 
 const server = Server.init(8080);
+server.app.use(bodyParser.json());
+server.app.use(bodyParser.urlencoded({ extended: true }));
 
 server.start(() => console.log('Servidor iniciado'));
