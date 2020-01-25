b149ac5c351a2d17c0670767d5483846c2620845
Omar Monrroy <ogmonrroy@gmail.com>
2020-01-25 03:38:59 -0600

    Server OOP

---
+++ src/server/server.ts
@@ -0,0 +1,30 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+
+export class App {
+  app: Application;
+
+  constructor(private port?: number | string) {
+    this.app = express();
+    this.settings();
+    this.middlewares();
+    this.routes();
+  }
+
+  private settings() {
+    this.app.set('port', this.port || process.env.PORT || 3000);
+  }
+
+  private middlewares() {
+    // TODO: Implementar dotenv para manejar variables de entorno.
+    this.app.use(morgan('dev'));
+    this.app.use(express.json());
+  }
+
+  private routes() {}
+
+  async listen(): Promise<void> {
+    await this.app.listen(this.app.get('port'));
+    console.log('Server on port', this.app.get('port'));
+  }
+}
